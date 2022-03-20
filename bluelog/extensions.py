4b61631bd50432a0eaafcef91853b4dcb3613709
MingruiWang2017 <1287658016@qq.com>
2022-03-20 17:12:37 +0800

    add csrf protect for non-wtforms

--- bluelog/extensions.py
+++ bluelog/extensions.py
@@ -4,10 +4,12 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 bootstrap = Bootstrap4()
 db = SQLAlchemy()
 login_manager = LoginManager()
+csrf = CSRFProtect()
 ckeditor = CKEditor()
 mail = Mail()
 moment = Moment()
